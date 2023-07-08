b20724ec78dfd34a54782998aab6eb702730ca95
Jgodka <125206831+Jgodka@users.noreply.github.com>
2023-07-08 18:30:09 +0000

    Deploying to gh-pages from @ Jgodka/goit-js-hw-09@699b564598280849b67474d62aa1ea2847d08aa1 🚀

---
+++ 01-color-switcher.f1ce028a.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),a=document.querySelector("body");let d=null;t.addEventListener("click",(function(t){t.target.disabled=!0,e.disabled=!1,d=setInterval((()=>{a.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(e){clearInterval(d),e.target.disabled=!0,t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.f1ce028a.js.map
