1ebe45e932515f61e35e74e505f8a55a544cfa1c
Jgodka <125206831+Jgodka@users.noreply.github.com>
2023-07-10 07:04:30 +0000

    Deploying to gh-pages from @ Jgodka/goit-js-hw-09@f8b48c8629c8f24199571adb39bd7ff25ebc3035 🚀

--- 01-color-switcher.07267a6c.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),a=document.querySelector("body"),n=null;t.addEventListener("click",(function(t){t.target.disabled=!0,e.disabled=!1,n=setInterval((function(){a.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(e){clearInterval(n),e.target.disabled=!0,t.disabled=!1}))}();
-//# sourceMappingURL=01-color-switcher.07267a6c.js.map
